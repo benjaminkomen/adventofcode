f8d19a53d813abece2048bf86330fe704259e4bd
Benjamin Komen <benjamin_komen@hotmail.com>
2017-12-01 22:12:57 +0100

    dag 1 gedaan

--- 2017/src/main/java/com/benjamin/Day1.java
+++ 2017/src/main/java/com/benjamin/Day1.java
@@ -2,7 +2,7 @@ package com.benjamin;
 
 import com.benjamin.repositories.InputRepository;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Day1 {
 
@@ -13,12 +13,74 @@ public class Day1 {
         Day1 instance = new Day1();
         String input = instance.inputRepository.getInput();
 
-        System.out.println(instance.bereken(input));
+        System.out.println("The answer to part 1 is: " + instance.deelEenA(input));
+        System.out.println("The answer to part 2 is: " + instance.deelTweeA(input));
     }
 
-    protected int bereken(String input) {
-        return Stream.of(input)
-                .mapToInt(Integer::new)
-                .sum();
+    protected int deelEenA(final String input) {
+        final int[] intArray = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
+        int sum = 0;
+
+        for (int i = 0; i < intArray.length; i++) {
+
+            // special case for the last element of the array
+            if (i == intArray.length - 1) {
+                if (intArray[i] == intArray[0]) {
+                    sum += intArray[i];
+                    break;
+                } else {
+                    break;
+                }
+            } else if (intArray[i] == intArray[i + 1]) { // add one element to the sum if two consecutive elements are equal
+                sum += intArray[i];
+            }
+        }
+
+        return sum;
+    }
+
+    /**
+     * The following method attempts to solve the puzzle using functional programming. It does not work yet, since the
+     * reduce operation works different than I need :/
+     */
+    protected int deelEenB(final String input) {
+        return Arrays.stream(input.split(""))
+                .mapToInt(Integer::parseInt)
+                .reduce((i, j) -> {
+                    if (i == j) {
+                        System.out.println("They are equal! i=" + i + " and j=" + j);
+                        return i;
+                    } else {
+                        return i;
+                    }
+                })
+                .orElse(0);
+    }
+
+    protected int deelTweeA(final String input) {
+        final int[] intArray = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
+        final int[] intArrayDoubleSize = combine(intArray, intArray);
+        int sum = 0;
+
+        for (int i = 0; i < intArrayDoubleSize.length / 2; i++) {
+
+            final int currentDigit = intArray[i];
+            final int positionComparingDigit = i + (intArray.length / 2);
+            final int comparingDigit = intArrayDoubleSize[positionComparingDigit];
+
+            if (currentDigit == comparingDigit) {
+                sum += currentDigit;
+            }
+        }
+
+        return sum;
+    }
+
+    public static int[] combine(int[] a, int[] b){
+        int length = a.length + b.length;
+        int[] result = new int[length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
     }
 }
--- 2017/src/test/java/com/benjamin/Day1Test.java
+++ 2017/src/test/java/com/benjamin/Day1Test.java
@@ -8,7 +8,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Day1Test {
 
-    private static final String TEST_INPUT_1 = "1122";
+    private static final String TEST_INPUT_DEEL_EEN_1 = "1122";
+    private static final String TEST_INPUT_DEEL_EEN_2 = "1111";
+    private static final String TEST_INPUT_DEEL_EEN_3 = "1234";
+    private static final String TEST_INPUT_DEEL_EEN_4 = "91212129";
+    private static final String TEST_INPUT_DEEL_TWEE_1 = "1212";
+    private static final String TEST_INPUT_DEEL_TWEE_2 = "1221";
+    private static final String TEST_INPUT_DEEL_TWEE_3 = "123425";
+    private static final String TEST_INPUT_DEEL_TWEE_4 = "123123";
+    private static final String TEST_INPUT_DEEL_TWEE_5 = "12131415";
 
     private Day1 target;
 
@@ -18,10 +26,65 @@ public class Day1Test {
     }
 
     @Test
-    public void testBereken1() {
-        int result1 = target.bereken(TEST_INPUT_1);
+    public void testDeelEen1() {
+        int result = target.deelEenA(TEST_INPUT_DEEL_EEN_1);
 
-        assertThat(result1, Matchers.is(3));
+        assertThat(result, Matchers.is(3));
+    }
 
+    @Test
+    public void testDeelEen2() {
+        int result = target.deelEenA(TEST_INPUT_DEEL_EEN_2);
+
+        assertThat(result, Matchers.is(4));
+    }
+
+    @Test
+    public void testDeelEen3() {
+        int result = target.deelEenA(TEST_INPUT_DEEL_EEN_3);
+
+        assertThat(result, Matchers.is(0));
+    }
+
+    @Test
+    public void testDeelEen4() {
+        int result = target.deelEenA(TEST_INPUT_DEEL_EEN_4);
+
+        assertThat(result, Matchers.is(9));
+    }
+
+    @Test
+    public void testDeelTwee1() {
+        int result = target.deelTweeA(TEST_INPUT_DEEL_TWEE_1);
+
+        assertThat(result, Matchers.is(6));
+    }
+
+    @Test
+    public void testDeelTwee2() {
+        int result = target.deelTweeA(TEST_INPUT_DEEL_TWEE_2);
+
+        assertThat(result, Matchers.is(0));
+    }
+
+    @Test
+    public void testDeelTwee3() {
+        int result = target.deelTweeA(TEST_INPUT_DEEL_TWEE_3);
+
+        assertThat(result, Matchers.is(4));
+    }
+
+    @Test
+    public void testDeelTwee4() {
+        int result = target.deelTweeA(TEST_INPUT_DEEL_TWEE_4);
+
+        assertThat(result, Matchers.is(12));
+    }
+
+    @Test
+    public void testDeelTwee5() {
+        int result = target.deelTweeA(TEST_INPUT_DEEL_TWEE_5);
+
+        assertThat(result, Matchers.is(4));
     }
 }
