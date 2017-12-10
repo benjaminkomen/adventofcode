a4eeb623c0ed33f2941e11a59976598c4e524240
Benjamin Komen <benjamin_komen@hotmail.com>
2017-12-10 17:46:38 +0100

    day 10 part 2 is done (hooray!)

--- 2017/src/main/java/com/benjamin/Day10.java
+++ 2017/src/main/java/com/benjamin/Day10.java
@@ -1,5 +1,7 @@
 package com.benjamin;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -15,7 +17,7 @@ public class Day10 {
         Day10 instance = new Day10();
 
         System.out.println("The answer to part 1 is: " + instance.deelEenA(INPUT, null));
-        System.out.println("The answer to part 2 is: " + instance.deelTweeA(INPUT));
+        System.out.println("The answer to part 2 is: " + instance.deelTweeA(INPUT, null));
     }
 
     /**
@@ -56,18 +58,111 @@ public class Day10 {
     }
 
     /**
-     *
+     * Returns the Knot Hash from an input string.
      */
-    protected int deelTweeA(final String input) {
-        return 0;
+    protected String deelTweeA(final String input, List<Integer> list) {
+
+        if (list == null) {
+            list = Stream.iterate(0, n -> n + 1)
+                    .limit(256)
+                    .collect(Collectors.toList());
+        }
+
+        int currentPosition = 0;
+        int skipSize = 0;
+        int counter = 0;
+
+        List<Byte> lengths = stringToBytes(input);
+        lengths.addAll(List.of(b(17), b(31), b(73), b(47), b(23)));
+
+        while (counter < 64) {
+            counter++;
+            for (Byte length : lengths) {
+
+                // rotate to zero, so the subList won't have to access indices out of bounds
+                Collections.rotate(list, -currentPosition);
+
+                // reverse the sublist
+                Collections.reverse(list.subList(0, length));
+
+                // rotate back to correctness
+                Collections.rotate(list, currentPosition);
+
+                // move current position
+                currentPosition = (currentPosition + length + skipSize) % list.size();
+
+                // increase skipSize
+                skipSize++;
+            }
+        }
+
+        List<Integer> denseHash = convertSparseHashToDenseHash(list);
+
+        return convertToHex(denseHash);
     }
 
     /**
-     * Turn a string into a list of strings.
+     * Turn a string into a list of integers.
      */
     private List<Integer> stringToIntegers(String input) {
         return Arrays.stream(input.split(","))
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * Turn a string into a list of bytes.
+     */
+    protected List<Byte> stringToBytes(String input) {
+        List<Byte> result = new ArrayList<>();
+        byte[] bytes = input.getBytes(StandardCharsets.US_ASCII);
+
+        for (byte b : bytes) {
+            result.add(b);
+        }
+
+        return result;
+    }
+
+    /**
+     * cast an int to a byte
+     */
+    private byte b(int i) {
+        return (byte) i;
+    }
+
+    /**
+     * Convert a sparse hash of 256 numbers to a dense hash of 16 numbers.
+     */
+    private List<Integer> convertSparseHashToDenseHash(List<Integer> list) {
+        List<List<Integer>> listOfLists = new ArrayList<>(list.stream()
+                .collect(Collectors.groupingBy(el -> list.indexOf(el) / 16))
+                .values());
+
+        List<Integer> result = new ArrayList<>();
+
+        for (List<Integer> subList : listOfLists) {
+            final Integer reduced = applyXOR(subList);
+            result.add(reduced);
+        }
+
+        return result;
+    }
+
+    /**
+     * Return a single number which is the XOR result of a list of given numbers.
+     */
+    protected Integer applyXOR(List<Integer> list) {
+        return list.stream()
+                .reduce(0, (a, b) -> a ^ b);
+    }
+
+    /**
+     * Convert a list of integers to a hexadecimal represented string.
+     */
+    protected String convertToHex(List<Integer> list) {
+        return list.stream()
+                .map(s -> String.format("%02x", s)) // note that Integer::toHexString won't work because it leaves out leading zeroes.
+                .collect(Collectors.joining());
     }
 }
--- 2017/src/test/java/com/benjamin/Day10Test.java
+++ 2017/src/test/java/com/benjamin/Day10Test.java
@@ -12,8 +12,13 @@ import static org.hamcrest.Matchers.is;
 
 public class Day10Test {
 
-    private static final String INPUT = "3,4,1,5";
     private static final List<Integer> INTEGER_LIST = new ArrayList<>(List.of(0, 1, 2, 3, 4));
+    private static final String INPUT_DEEL1 = "3,4,1,5";
+    private static final String INPUT_DEEL2_0 = "1,2,3";
+    private static final String INPUT_DEEL2_1 = "";
+    private static final String INPUT_DEEL2_2 = "AoC 2017";
+    private static final String INPUT_DEEL2_3 = "1,2,3";
+    private static final String INPUT_DEEL2_4 = "1,2,4";
     private Day10 target;
 
     @Before
@@ -23,17 +28,64 @@ public class Day10Test {
 
     @Test
     public void testDeelEen() {
-        int result = target.deelEenA(INPUT, INTEGER_LIST);
+        int result = target.deelEenA(INPUT_DEEL1, INTEGER_LIST);
 
         assertThat(result, is(12));
     }
 
     @Ignore
     @Test
-    public void testDeelTwee() {
-        int result = target.deelTweeA(INPUT);
+    public void testDeelTwee_0() {
+        String result = target.deelTweeA(INPUT_DEEL1, null);
 
-        assertThat(result, is(99));
+        assertThat(result, is("???"));
     }
 
+    @Test
+    public void testDeelTwee_1() {
+        String result = target.deelTweeA(INPUT_DEEL2_1, null);
+
+        assertThat(result, is("a2582a3a0e66e6e86e3812dcb672a272"));
+    }
+
+    @Test
+    public void testDeelTwee_2() {
+        String result = target.deelTweeA(INPUT_DEEL2_2, null);
+
+        assertThat(result, is("33efeb34ea91902bb2f59c9920caa6cd"));
+    }
+
+    @Test
+    public void testDeelTwee_3() {
+        String result = target.deelTweeA(INPUT_DEEL2_3, null);
+
+        assertThat(result, is("3efbe78a8d82f29979031a4aa0b16a9d"));
+    }
+
+    @Test
+    public void testDeelTwee_4() {
+        String result = target.deelTweeA(INPUT_DEEL2_4, null);
+
+        assertThat(result, is("63960835bcdc130f0b66d7ff4f6a5a8e"));
+    }
+
+    @Test
+    public void testStringToBytes() {
+        assertThat(target.stringToBytes("1,2,3"), is(List.of(b(49), b(44), b(50), b(44), b(51))));
+    }
+
+    @Test
+    public void testApplyXOR() {
+        assertThat(target.applyXOR(List.of(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)), is(64));
+    }
+
+    @Test
+    public void testConvertToHex() {
+        assertThat(target.convertToHex(List.of(64, 7, 255)), is("4007ff"));
+    }
+
+    // cast an int to a byte
+    private byte b(int i) {
+        return (byte) i;
+    }
 }
