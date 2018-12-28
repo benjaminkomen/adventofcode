c31a5371e8b27746232f2de88bfb1c08d551b533
Benjamin Komen <benjamin_komen@hotmail.com>
2018-12-28 12:01:51 +0100

    preparing some test classes. Starting with day 3

--- 2018/src/main/java/com/benjamin/Day3.java
+++ 2018/src/main/java/com/benjamin/Day3.java
@@ -2,21 +2,20 @@ package com.benjamin;
 
 public class Day3 extends Day {
 
-    private static final int INPUT = 0;
-
     public static void main(String[] args) {
 
         Day3 instance = new Day3();
+        String input = instance.inputRepository.getInput(3);
 
-        System.out.println("The answer to part 1 is: " + instance.deelEenA(INPUT));
-        System.out.println("The answer to part 2 is: " + instance.deelTweeA(INPUT));
+        System.out.println("The answer to part 1 is: " + instance.deelEenA(input));
+        System.out.println("The answer to part 2 is: " + instance.deelTweeA(input));
     }
 
-    protected int deelEenA(final int input) {
+    protected int deelEenA(final String input) {
         return 0;
     }
 
-    protected int deelTweeA(final int input) {
+    protected int deelTweeA(final String input) {
         return 0;
     }
 }
---
+++ 2018/src/test/java/com/benjamin/Day7Test.java
@@ -0,0 +1,32 @@
+package com.benjamin;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Day7Test {
+
+    private Day7 target;
+    private static final String INPUT1 = "";
+
+    @BeforeEach
+    public void setup() {
+        target = new Day7();
+    }
+
+    @Test
+    public void testDeelEen1() {
+        int result = target.deelEenA(INPUT1);
+
+        assertThat(result, Matchers.is(-1));
+    }
+
+    @Test
+    public void testDeelTwee1() {
+        int result = target.deelTweeA(INPUT1);
+
+        assertThat(result, Matchers.is(-1));
+    }
+}
