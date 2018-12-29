e527f2fef8a87bb4d68e8e245b7530ac5f99bb9e
Benjamin Komen <benjamin_komen@hotmail.com>
2018-12-29 12:58:44 +0100

    finished day 4, part 1

--- 2018/src/main/java/com/benjamin/Day4.java
+++ 2018/src/main/java/com/benjamin/Day4.java
@@ -1,21 +1,187 @@
 package com.benjamin;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Day4 extends Day {
+
+    private static final Pattern PATTERN = Pattern.compile("^\\[(.*?)] (wakes up|Guard #(\\d+) begins shift|falls asleep)$");
+    private static final DateTimeFormatter LOCALDATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private List<Record> records = new ArrayList<>();
 
     public static void main(String[] args) {
 
         Day4 instance = new Day4();
         String input = instance.inputRepository.getInput(4);
 
-        System.out.println("The answer to part 1 is: " + instance.deelEenA(input));
+        System.out.println("The answer to part 1 is: " + instance.deelEenA(input)); // 35623
         System.out.println("The answer to part 2 is: " + instance.deelTweeA(input));
     }
 
     protected int deelEenA(final String input) {
-        return 0;
+        this.records = makeRecords(input);
+        this.records = addMissingGuardIds(this.records);
+        Map<Integer, Map<Integer, Integer>> minutesAsleepPerGuardId = determineMinutesAsleepPerGuardId(this.records);
+        Integer mostAsleepGuardId = getMostAsleepGuardId(minutesAsleepPerGuardId);
+        Integer minuteMostAsleep = getMinuteMostAsleep(minutesAsleepPerGuardId, mostAsleepGuardId);
+        return mostAsleepGuardId * minuteMostAsleep;
+    }
+
+    @NotNull
+    private List<Record> makeRecords(String input) {
+        return Arrays.stream(input.split("\n"))
+                .map(Record::new)
+                .sorted(Comparator.comparing(Record::getTimestamp))
+                .collect(Collectors.toList());
+    }
+
+    private List<Record> addMissingGuardIds(List<Record> records) {
+        List<Record> newRecords = new ArrayList<>(records); // shallow copy only
+        for (int i = 0; i < newRecords.size(); i++) {
+            Record record = newRecords.get(i);
+            if (record.getGuardId() == null) {
+                record.guardId = records.subList(0, i).stream()
+                        .sorted(Collections.reverseOrder(Comparator.comparing(Record::getTimestamp)))
+                        .map(Record::getGuardId)
+                        .filter(Objects::nonNull)
+                        .findFirst()
+                        .orElse(null);
+            }
+        }
+
+        return newRecords;
+    }
+
+    /**
+     * Map of: GuardId with a Map of minute and frequency of being asleep during that minute
+     */
+    private Map<Integer, Map<Integer, Integer>> determineMinutesAsleepPerGuardId(List<Record> records) {
+        Map<Integer, Map<Integer, Integer>> minutesAsleepPerGuardId = new HashMap<>();
+
+        // loop over the records and capture consecutive FallAsleep and WakeUp actions
+        for (int i = 0; i < records.size(); i++) {
+            Record record = records.get(i);
+            if (record.getAction() == GuardAction.WakeUp) {
+                Record previousRecord = records.get(i - 1);
+                Integer guardId = record.getGuardId();
+
+                // loop over the minutes a guard is asleep and add them to the map
+                for (int j = previousRecord.getTimestamp().getMinute(); j < record.getTimestamp().getMinute(); j++) {
+                    Map<Integer, Integer> frequencyAsleepPerMinute = new HashMap<>();
+                    frequencyAsleepPerMinute.put(j, 1);
+                    minutesAsleepPerGuardId.merge(guardId, frequencyAsleepPerMinute,
+                            (oldFreqMap, newFreqMap) -> {
+                                newFreqMap.forEach((k, v) -> {
+                                    if (!oldFreqMap.containsKey(k)) {
+                                        // this minute is not registered yet, add it
+                                        oldFreqMap.put(k, v);
+                                    } else {
+                                        // this minute was already registered, add the new value to the old value
+                                        oldFreqMap.merge(k, v, (oldVal, newVal) -> oldVal + newVal);
+                                    }
+                                });
+                                return oldFreqMap;
+                            });
+                }
+            }
+        }
+
+        return minutesAsleepPerGuardId;
+    }
+
+    /**
+     * Calculate the sum of all minutes each guard is asleep, and return the guard ID of the guard with the maximum
+     * amount of minutes asleep.
+     */
+    protected Integer getMostAsleepGuardId(Map<Integer, Map<Integer, Integer>> minutesAsleepPerGuardId) {
+        return Collections.max(minutesAsleepPerGuardId.entrySet(),
+                Comparator.comparingLong(e -> e.getValue().values().stream()
+                        .mapToLong(Long::valueOf)
+                        .sum()))
+                .getKey();
+    }
+
+    protected Integer getMinuteMostAsleep(Map<Integer, Map<Integer, Integer>> minutesAsleepPerGuardId,
+                                          Integer mostAsleepGuardId) {
+        return minutesAsleepPerGuardId.entrySet().stream()
+                .filter(e -> e.getKey().equals(mostAsleepGuardId))
+                .map(Map.Entry::getValue)
+                .map(map -> Collections.max(map.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey())
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Could not determine minute most asleep"));
     }
 
     protected int deelTweeA(final String input) {
         return 0;
+    }
+
+    private static class Record {
+
+        private final LocalDateTime timestamp;
+        private Integer guardId;
+        private final GuardAction action;
+
+        private Record() {
+            this.timestamp = null;
+            this.guardId = null;
+            this.action = null;
+        }
+
+        public Record(LocalDateTime timestamp, Integer guardId, GuardAction action) {
+            this.timestamp = timestamp;
+            this.guardId = guardId;
+            this.action = action;
+        }
+
+        public Record(String input) {
+            Matcher matcher = PATTERN.matcher(input);
+
+            if (matcher.matches()) {
+                this.timestamp = LocalDateTime.parse(matcher.group(1), LOCALDATETIME_FORMAT);
+                this.guardId = matcher.group(3) != null ? Integer.valueOf(matcher.group(3)) : null;
+                this.action = GuardAction.fromString(matcher.group(2));
+            } else {
+                this.timestamp = null;
+                this.guardId = null;
+                this.action = null;
+            }
+        }
+
+        public LocalDateTime getTimestamp() {
+            return timestamp;
+        }
+
+        public Integer getGuardId() {
+            return guardId;
+        }
+
+        public GuardAction getAction() {
+            return action;
+        }
+    }
+
+    private enum GuardAction {
+        BeginShift,
+        FallAsleep,
+        WakeUp;
+
+        public static GuardAction fromString(String input) {
+            if (input.startsWith("Guard")) {
+                return BeginShift;
+            } else if ("falls asleep".equals(input)) {
+                return FallAsleep;
+            } else if ("wakes up".equals(input)) {
+                return WakeUp;
+            } else {
+                throw new IllegalStateException(String.format("Input %s could not be mapped to BeginShift," +
+                        " FallAsleep or WakeUp.", input));
+            }
+        }
     }
 }
--- 2018/src/test/java/com/benjamin/Day4Test.java
+++ 2018/src/test/java/com/benjamin/Day4Test.java
@@ -1,15 +1,65 @@
 package com.benjamin;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class Day4Test {
 
     private Day4 target;
-    private static final String INPUT1 = "";
+    private static final String INPUT1 =
+            "[1518-11-01 00:00] Guard #10 begins shift\n" +
+            "[1518-11-01 00:05] falls asleep\n" +
+            "[1518-11-01 00:25] wakes up\n" +
+            "[1518-11-01 00:30] falls asleep\n" +
+            "[1518-11-01 00:55] wakes up\n" +
+            "[1518-11-01 23:58] Guard #99 begins shift\n" +
+            "[1518-11-02 00:40] falls asleep\n" +
+            "[1518-11-02 00:50] wakes up\n" +
+            "[1518-11-03 00:05] Guard #10 begins shift\n" +
+            "[1518-11-03 00:24] falls asleep\n" +
+            "[1518-11-03 00:29] wakes up\n" +
+            "[1518-11-04 00:02] Guard #99 begins shift\n" +
+            "[1518-11-04 00:36] falls asleep\n" +
+            "[1518-11-04 00:46] wakes up\n" +
+            "[1518-11-05 00:03] Guard #99 begins shift\n" +
+            "[1518-11-05 00:45] falls asleep\n" +
+            "[1518-11-05 00:55] wakes up\n";
+    private static final String INPUT2 =
+            "[1518-02-15 23:57] Guard #433 begins shift\n" +
+            "[1518-02-16 00:28] falls asleep\n" +
+            "[1518-02-16 00:45] wakes up\n" +
+            "[1518-02-16 00:50] falls asleep\n" +
+            "[1518-02-16 00:55] wakes up\n" +
+            "[1518-02-17 00:00] Guard #677 begins shift\n" +
+            "[1518-02-17 00:38] falls asleep\n" +
+            "[1518-02-17 00:42] wakes up\n" +
+            "[1518-02-18 00:02] Guard #277 begins shift\n" +
+            "[1518-02-18 00:06] falls asleep\n" +
+            "[1518-02-18 00:57] wakes up\n" +
+            "[1518-02-19 00:01] Guard #109 begins shift\n" +
+            "[1518-02-19 00:21] falls asleep\n" +
+            "[1518-02-19 00:34] wakes up\n" +
+            "[1518-02-20 00:02] Guard #239 begins shift\n" +
+            "[1518-02-20 00:13] falls asleep\n" +
+            "[1518-02-20 00:48] wakes up\n" +
+            "[1518-02-20 23:56] Guard #1097 begins shift\n" +
+            "[1518-02-21 00:08] falls asleep\n" +
+            "[1518-02-21 00:17] wakes up\n" +
+            "[1518-02-21 00:21] falls asleep\n" +
+            "[1518-02-21 00:45] wakes up\n" +
+            "[1518-02-21 23:57] Guard #677 begins shift\n" +
+            "[1518-02-22 00:18] falls asleep\n" +
+            "[1518-02-22 00:38] wakes up\n" +
+            "[1518-02-22 00:47] falls asleep\n" +
+            "[1518-02-22 00:48] wakes up\n" +
+            "[1518-02-22 00:56] falls asleep\n" +
+            "[1518-02-22 00:58] wakes up";
 
     @BeforeEach
     public void setup() {
@@ -20,13 +70,44 @@ public class Day4Test {
     public void testDeelEen1() {
         int result = target.deelEenA(INPUT1);
 
-        assertThat(result, Matchers.is(-1));
+        assertThat(result, is(240));
+    }
+
+    @Test
+    public void testDeelEen2() {
+        int result = target.deelEenA(INPUT2);
+
+        assertThat(result, is(1662));
+    }
+
+    /**
+     * Guard 1 is 6 minutes asleep, guard 2 is 9 minutes asleep; expected guard 2
+     */
+    @Test
+    public void testGetMostAsleepGuardId() {
+        Map<Integer, Integer> minutesAsleep1 = Map.of(1,1,2,1,3,1,4,1,5,2);
+        Map<Integer, Integer> minutesAsleep2 = Map.of(1,2,2,3,3,2,4,1, 5,1);
+        Map<Integer, Map<Integer, Integer>> map = Map.of(1, minutesAsleep1, 2, minutesAsleep2);
+        Integer result = target.getMostAsleepGuardId(map);
+        assertThat(result, is(2));
+    }
+
+    /**
+     * Guard 1 is most asleep during minute 5
+     */
+    @Test
+    public void testGetMinuteMostAsleep() {
+        Map<Integer, Integer> minutesAsleep1 = Map.of(1,1,2,1,3,1,4,1,5,2);
+        Map<Integer, Integer> minutesAsleep2 = Map.of(1,2,2,3,3,2,4,1);
+        Map<Integer, Map<Integer, Integer>> map = Map.of(1, minutesAsleep1, 2, minutesAsleep2);
+        Integer result = target.getMinuteMostAsleep(map, 1);
+        assertThat(result, is(5));
     }
 
     @Test
     public void testDeelTwee1() {
         int result = target.deelTweeA(INPUT1);
 
-        assertThat(result, Matchers.is(-1));
+        assertThat(result, is(-1));
     }
 }
