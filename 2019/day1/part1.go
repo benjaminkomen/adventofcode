e2530d64cb90758bfaf32a69be299fc990a638a7
Benjamin Komen <benjamin.komen@duxxie.nl>
2019-12-01 10:52:44 +0100

    Day 1

--- 2019/day1/part1.go
+++ 2019/day1/part1.go
@@ -1,9 +1,53 @@
 package main
 
-import "github.com/adventofcode/2019/common"
+import (
+	"fmt"
+	"github.com/adventofcode/2019/common"
+	"os"
+	"strconv"
+)
 
 func main() {
 	scanner := common.ReadLines("./day1/input.txt")
 
-	scanner.Text()
+	var totalFuelRequired int64          // part 1
+	var totalRecursiveFuelRequired int64 // part 2
+
+	for scanner.Scan() {
+		inputNumber, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Printf("could not convert string to number: %s", err)
+			os.Exit(-1)
+		}
+
+		fuelRequired := CalculateFuelRequired(inputNumber)
+		totalFuelRequired += int64(fuelRequired)
+
+		recursiveFuelRequired := CalculateRecursiveFuelRequired(inputNumber)
+		totalRecursiveFuelRequired += int64(recursiveFuelRequired)
+	}
+
+	fmt.Printf("Part 1: total fuel required is: %d \n", totalFuelRequired)
+	fmt.Printf("Part 2: total recursive fuel required is: %d \n", totalRecursiveFuelRequired)
+
+}
+
+func CalculateRecursiveFuelRequired(moduleMass int) int {
+
+	fuelRequired := CalculateFuelRequired(moduleMass)
+
+	// Any mass that would require negative fuel should instead be treated as if it requires zero fuel
+	if fuelRequired <= 0 {
+		return 0
+	}
+
+	return fuelRequired + CalculateRecursiveFuelRequired(fuelRequired)
+}
+
+/*
+Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+*/
+func CalculateFuelRequired(moduleMass int) int {
+	dividedMass := moduleMass / 3 // NOTE: Go automatically rounds this down, the result is an int
+	return dividedMass - 2
 }
--- 2019/day1/part1_test.go
+++ 2019/day1/part1_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestCalculateFuelRequired(t *testing.T) {
+	assert.Equal(t, 2, CalculateFuelRequired(12))
+	assert.Equal(t, 2, CalculateFuelRequired(14))
+	assert.Equal(t, 654, CalculateFuelRequired(1969))
+	assert.Equal(t, 33583, CalculateFuelRequired(100756))
+}
+
+func TestCalculateRecursiveFuelRequired(t *testing.T) {
+	assert.Equal(t, 2, CalculateRecursiveFuelRequired(14))
+	assert.Equal(t, 966, CalculateRecursiveFuelRequired(1969))
+	assert.Equal(t, 50346, CalculateRecursiveFuelRequired(100756))
+}
