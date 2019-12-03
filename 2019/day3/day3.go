a2e72524feded9f57e31740ed63ff89f7250a65e
Benjamin Komen <benjamin.komen@duxxie.nl>
2019-12-03 08:27:31 +0100

    Day 3, part 2

--- 2019/day3/day3.go
+++ 2019/day3/day3.go
@@ -15,7 +15,7 @@ func main() {
 	inputNumbers := PrepareInput(inputAsStr)
 
 	result1 := RunProgram(inputNumbers)
-	result2 := 0
+	result2 := RunProgram2(inputNumbers)
 
 	fmt.Printf("Part 1 is: %d \n", result1)
 	fmt.Printf("Part 2 is: %d \n", result2)
@@ -52,9 +52,19 @@ const (
 	Wire_both
 )
 
+type WireAndSteps struct {
+	Wire  Wire
+	Steps int
+}
+
+type intersectionTravelled struct {
+	coordinate
+	steps int
+}
+
 func RunProgram(input map[int][]instruction) int {
-	grid := make(map[coordinate]Wire) // every position can have: 0 (nothing), 1 (the first wire), 2 (the second wire) or 3 (both wires)
-	var intersections []coordinate    // locations where both wires cross
+	grid := make(map[coordinate]WireAndSteps) // every position can have: 0 (nothing), 1 (the first wire), 2 (the second wire) or 3 (both wires)
+	var intersections []intersectionTravelled // locations where both wires cross
 	var currentCoordinate coordinate
 
 	for w, instructions := range input {
@@ -66,11 +76,33 @@ func RunProgram(input map[int][]instruction) int {
 		}
 
 		for i, instruction := range instructions {
-			grid, currentCoordinate, intersections = instruction.apply(grid, intersections, currentCoordinate, wire, i)
+			grid, currentCoordinate, intersections, _ = instruction.apply(grid, intersections, 0, currentCoordinate, wire, i)
 		}
 	}
 
 	return closestToOrigin(intersections)
+}
+
+func RunProgram2(input map[int][]instruction) int {
+	grid := make(map[coordinate]WireAndSteps) // every position can have: 0 (nothing), 1 (the first wire), 2 (the second wire) or 3 (both wires)
+	var intersections []intersectionTravelled // locations where both wires cross
+	var currentCoordinate coordinate
+	var stepsTravelled int
+
+	for w, instructions := range input {
+		var wire Wire
+		if w == 0 {
+			wire = Wire_first
+		} else if w == 1 {
+			wire = Wire_second
+		}
+
+		for i, instruction := range instructions {
+			grid, currentCoordinate, intersections, stepsTravelled = instruction.apply(grid, intersections, stepsTravelled, currentCoordinate, wire, i)
+		}
+	}
+
+	return leastTravelled(intersections)
 }
 
 type instruction struct {
@@ -78,13 +110,14 @@ type instruction struct {
 	amount    int
 }
 
-func (instruction instruction) apply(grid map[coordinate]Wire, intersections []coordinate, currentCoordinate coordinate, wire Wire, instructionNumber int) (map[coordinate]Wire, coordinate, []coordinate) {
+func (instruction instruction) apply(grid map[coordinate]WireAndSteps, intersections []intersectionTravelled, travelled int, currentCoordinate coordinate, wire Wire, instructionNumber int) (map[coordinate]WireAndSteps, coordinate, []intersectionTravelled, int) {
 	// set current coordinate to the origin when starting with a new wire
 	if instructionNumber == 0 {
 		currentCoordinate = coordinate{
 			x: 0,
 			y: 0,
 		}
+		travelled = 0
 	}
 
 	for i := 0; i < instruction.amount; i++ {
@@ -102,21 +135,37 @@ func (instruction instruction) apply(grid map[coordinate]Wire, intersections []c
 			os.Exit(-1)
 		}
 
+		travelled++
+
 		wireAtCurrentLocation := grid[currentCoordinate]
-		if wireAtCurrentLocation == Wire_nothing {
-			grid[currentCoordinate] = wire
-		} else if wireAtCurrentLocation == wire {
+		if wireAtCurrentLocation.Wire == Wire_nothing {
+			grid[currentCoordinate] = WireAndSteps{
+				Wire:  wire,
+				Steps: travelled,
+			}
+		} else if wireAtCurrentLocation.Wire == wire {
 			// do nothing, we are crossing ourselves
-		} else if wireAtCurrentLocation != wire {
-			grid[currentCoordinate] = Wire_both
-			intersections = append(intersections, currentCoordinate) // this is what we are actually interested in
+		} else if wireAtCurrentLocation.Wire != wire {
+
+			existingGridElement := grid[currentCoordinate]
+
+			grid[currentCoordinate] = WireAndSteps{
+				Wire:  Wire_both,
+				Steps: existingGridElement.Steps,
+			}
+
+			it := intersectionTravelled{
+				coordinate: currentCoordinate,
+				steps:      grid[currentCoordinate].Steps + travelled, // steps by first wire and steps by second wire
+			}
+			intersections = append(intersections, it) // this is what we are actually interested in
 		}
 	}
 
-	return grid, currentCoordinate, intersections
+	return grid, currentCoordinate, intersections, travelled
 }
 
-func closestToOrigin(intersections []coordinate) int {
+func closestToOrigin(intersections []intersectionTravelled) int {
 	var manhattanDistances []int
 
 	for _, intersection := range intersections {
@@ -128,6 +177,22 @@ func closestToOrigin(intersections []coordinate) int {
 
 	if len(manhattanDistances) > 0 {
 		return manhattanDistances[0]
+	}
+	return 0
+}
+
+func leastTravelled(intersections []intersectionTravelled) int {
+
+	var travelDistances []int
+
+	for _, intersection := range intersections {
+		travelDistances = append(travelDistances, intersection.steps)
+	}
+
+	sort.Ints(travelDistances) // ascending, lowest number first
+
+	if len(travelDistances) > 0 {
+		return travelDistances[0]
 	}
 	return 0
 }
--- 2019/day3/day3_test.go
+++ 2019/day3/day3_test.go
@@ -10,3 +10,9 @@ func TestRunProgram(t *testing.T) {
 	assert.Equal(t, 159, RunProgram(PrepareInput("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")))
 	assert.Equal(t, 135, RunProgram(PrepareInput("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
 }
+
+func TestRunProgram2(t *testing.T) {
+	assert.Equal(t, 30, RunProgram2(PrepareInput("R8,U5,L5,D3\nU7,R6,D4,L4")))
+	assert.Equal(t, 610, RunProgram2(PrepareInput("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")))
+	assert.Equal(t, 410, RunProgram2(PrepareInput("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
+}
