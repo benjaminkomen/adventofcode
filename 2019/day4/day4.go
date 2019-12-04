d8a5a72ee7aa54be7db3c22fe4a8ef605dfd3a2d
Benjamin Komen <benjamin.komen@duxxie.nl>
2019-12-04 10:25:44 +0100

    Day 4, part 2

--- 2019/day4/day4.go
+++ 2019/day4/day4.go
@@ -10,10 +10,10 @@ func main() {
 	inputLower, inputUpper := 240920, 789857
 
 	result1 := RunProgram(inputLower, inputUpper)
-	//result2 := RunProgram2(inputNumbers)
+	result2 := RunProgram2(inputLower, inputUpper)
 
 	fmt.Printf("Part 1 is: %d \n", result1)
-	//fmt.Printf("Part 2 is: %d \n", result2)
+	fmt.Printf("Part 2 is: %d \n", result2)
 
 }
 
@@ -30,9 +30,27 @@ func RunProgram(inputLower, inputUpper int) int {
 	return numberOfPasswords
 }
 
+func RunProgram2(inputLower, inputUpper int) int {
+
+	numberOfPasswords := 0
+
+	for i := inputLower; i < (inputUpper + 1); i++ {
+		if MeetsConditions2(i) {
+			numberOfPasswords++
+		}
+	}
+
+	return numberOfPasswords
+}
+
 func MeetsConditions(number int) bool {
 	numberSliced := intToIntSlice(number)
 	return TwoSameAdjacentDigits(numberSliced) && DigitsNeverDecrease(numberSliced)
+}
+
+func MeetsConditions2(number int) bool {
+	numberSliced := intToIntSlice(number)
+	return TwoSameAdjacentDigitsNotPartOfLargerGroup(numberSliced) && DigitsNeverDecrease(numberSliced)
 }
 
 func TwoSameAdjacentDigits(numberSliced []int) bool {
@@ -45,13 +63,43 @@ func TwoSameAdjacentDigits(numberSliced []int) bool {
 	return false
 }
 
+/* The point is: if we find ANY pair of adjacent equal digits, we must make sure that:
+- the digit before the pair is not the same as the pair
+- the digit after the pair is not the same as the pair
+
+Then we are good to go, the rest of the digits don't matter anymore.
+*/
+func TwoSameAdjacentDigitsNotPartOfLargerGroup(numberSliced []int) bool {
+	for i := 0; i < (len(numberSliced) - 1); i++ {
+		if numberSliced[i] == numberSliced[i+1] && digitBeforePairIsNotTheSame(i, numberSliced) && digitAfterPairIsNotTheSame(i, numberSliced) {
+			return true
+		}
+	}
+	return false
+}
+
+func digitBeforePairIsNotTheSame(i int, numberSliced []int) bool {
+	if i > 0 {
+		// we are not at the first digit, so we can safely look one digit to the left
+		return numberSliced[i-1] != numberSliced[i]
+	}
+	return true // there is no digit to the left
+}
+
+func digitAfterPairIsNotTheSame(i int, numberSliced []int) bool {
+	if i < (len(numberSliced) - 2) {
+		// we are not at the but-last digit, so we can safely look two digits to the right
+		return numberSliced[i+2] != numberSliced[i+1]
+	}
+	return true // there is no digit two places to the right
+}
+
 func DigitsNeverDecrease(numberSliced []int) bool {
 	for i := 0; i < (len(numberSliced) - 1); i++ {
 		if numberSliced[i+1] < numberSliced[i] {
 			return false
 		}
 	}
-
 	return true
 }
 
--- 2019/day4/day4_test.go
+++ 2019/day4/day4_test.go
@@ -18,3 +18,18 @@ func TestRunProgram(t *testing.T) {
 
 	assert.Equal(t, 3, RunProgram(123456, 123488)) // 123466, 123477, 123488
 }
+
+func TestRunProgram2(t *testing.T) {
+	assert.Equal(t, true, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 1, 2, 3, 3, 3}))
+	assert.Equal(t, false, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 1, 1, 4, 5, 6}))
+	assert.Equal(t, true, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 2, 2, 4, 5, 6}))
+	assert.Equal(t, false, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 2, 2, 2, 5, 6}))
+	assert.Equal(t, true, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 2, 3, 4, 5, 5}))
+	assert.Equal(t, false, TwoSameAdjacentDigitsNotPartOfLargerGroup([]int{1, 2, 3, 5, 5, 5}))
+
+	assert.Equal(t, true, MeetsConditions2(112233))
+	assert.Equal(t, false, MeetsConditions2(123444))
+	assert.Equal(t, true, MeetsConditions2(111122))
+
+	assert.Equal(t, 3, RunProgram2(123456, 123488)) // 123466, 123477, 123488
+}
