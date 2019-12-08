807f5a2d7593573449a1d377080413a0e4f988dc
Benjamin Komen <komenb@gmail.com>
2019-12-08 16:26:33 +0100

    Day 8, part 2 (pre-restructuring)

--- 2019/day8/day8.go
+++ 2019/day8/day8.go
@@ -11,16 +11,14 @@ import (
 func main() {
 	inputAsStr := string(common.ReadBytes("./day8/input.txt"))
 
-	result1 := RunProgram(inputAsStr, 25, 6)
-	//result2 := RunProgram(inputAsStr, 25, 6)
+	result1 := RunProgram(MakeLayeredImage(inputAsStr, 25, 6))
+	result2 := RunProgram2(MakeLayeredImage(inputAsStr, 25, 6))
 
 	fmt.Printf("Part 1 is: %d \n", result1)
-	//fmt.Printf("Part 2 is: %d \n", result2)
+	fmt.Printf("Part 2 is: %d \n", result2)
 }
 
-func RunProgram(input string, imageWidth int, imageHeight int) int {
-	layerArea := imageWidth * imageHeight
-	imageLayers := makeLayeredImage(input, layerArea)
+func RunProgram(imageLayers LayeredImage) int {
 	imageLayerWithFewestZeroDigits := findLayerWithMinimum(imageLayers, 0)
 	numberOfOneDigits := countNumberInLayer(imageLayerWithFewestZeroDigits, 1)
 	numberOfTwoDigits := countNumberInLayer(imageLayerWithFewestZeroDigits, 2)
@@ -28,13 +26,21 @@ func RunProgram(input string, imageWidth int, imageHeight int) int {
 	return numberOfOneDigits * numberOfTwoDigits
 }
 
-type layeredImage [][]int
+func RunProgram2(imageLayers LayeredImage) []int {
+	decodedImage := decodeImage(imageLayers)
+	visualiseImage(decodedImage)
+	return decodedImage
+}
 
-func makeLayeredImage(input string, layerArea int) layeredImage {
+type Image []int
+type LayeredImage []Image
+
+func MakeLayeredImage(input string, imageWidth int, imageHeight int) LayeredImage {
+	layerArea := imageWidth * imageHeight
 	layerCounter := 0
 	inputAsSlice := strings.Split(input, "")
 	amountOfLayers := len(inputAsSlice) / layerArea
-	var result = make([][]int, amountOfLayers)
+	var result = make(LayeredImage, amountOfLayers)
 
 	for i, digit := range inputAsSlice {
 		digitAsInt, err := strconv.Atoi(digit)
@@ -56,21 +62,21 @@ func makeLayeredImage(input string, layerArea int) layeredImage {
 	return result
 }
 
-func findLayerWithMinimum(layers layeredImage, number int) []int {
+func findLayerWithMinimum(layers LayeredImage, number int) Image {
 	currentMinimumAmount := int(^uint(0) >> 1)
-	var currentLayer []int
+	var currentImageLayer Image
 
 	for _, layer := range layers {
 		currentAmount := countNumberInLayer(layer, number)
 		if currentAmount < currentMinimumAmount {
-			currentLayer = layer
+			currentImageLayer = layer
 			currentMinimumAmount = currentAmount
 		}
 	}
-	return currentLayer
+	return currentImageLayer
 }
 
-func countNumberInLayer(layer []int, number int) int {
+func countNumberInLayer(layer Image, number int) int {
 	var amount int
 
 	for _, digit := range layer {
@@ -79,4 +85,22 @@ func countNumberInLayer(layer []int, number int) int {
 		}
 	}
 	return amount
+}
+
+func decodeImage(layers LayeredImage) Image {
+	decodedImage := layers[0]
+
+	for _, layer := range layers {
+		for pos, pixel := range layer {
+			// replace transparent upper layer with current layer's pixel, otherwise leave the upper layer as the visible pixel
+			if decodedImage[pos] == 2 {
+				decodedImage[pos] = pixel
+			}
+		}
+	}
+	return decodedImage
+}
+
+func visualiseImage(image Image) {
+
 }
--- 2019/day8/day8_test.go
+++ 2019/day8/day8_test.go
@@ -6,5 +6,9 @@ import (
 )
 
 func TestRunProgram(t *testing.T) {
-	assert.Equal(t, 1, RunProgram("123456789012", 3, 2))
+	assert.Equal(t, 1, RunProgram(MakeLayeredImage("123456789012", 3, 2)))
+}
+
+func TestRunProgram2(t *testing.T) {
+	assert.Equal(t, []int{0, 1, 1, 0}, RunProgram2(MakeLayeredImage("0222112222120000", 2, 2)))
 }
