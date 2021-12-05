6b924f6c2b2fcada80b80a7f8e38a0ac78285388
Benjamin Komen <benjamin_komen@hotmail.com>
2021-12-05 13:47:24 +0100

    Finished day 1 - part 2

--- 2021/src/main/kotlin/Day1.kt
+++ 2021/src/main/kotlin/Day1.kt
@@ -6,6 +6,9 @@ object Day1 {
     val input = this::class.java.getResource("/input/day1.txt")?.readText() ?: ""
     val result1 = part1(input)
     println("Result for Day1 - part 1: $result1")
+
+    val result2 = part2(input)
+    println("Result for Day1 - part 2: $result2")
   }
 
   fun part1(input: String): Int {
@@ -14,6 +17,15 @@ object Day1 {
       .mapNotNull { it.toIntOrNull() }
       .asSequence()
       .windowed(2) { (left, right) -> if (right > left) 1 else 0 }
+      .sumOf { it }
+  }
+
+  fun part2(input: String): Int {
+    return input
+      .split(Regex.fromLiteral("\n"))
+      .mapNotNull { it.toIntOrNull() }
+      .asSequence()
+      .windowed(4) { (one, two, three, four) -> if (two + three + four > one + two + three) 1 else 0 }
       .sumOf { it }
   }
 }
--- 2021/src/test/kotlin/Day1Test.kt
+++ 2021/src/test/kotlin/Day1Test.kt
@@ -1,11 +1,9 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day1Test {
 
-    @Test
-    fun `should test part1`() {
-      val testInput = """199
+  private val testInput = """199
 200
 208
 210
@@ -16,6 +14,17 @@ internal class Day1Test {
 260
 263"""
 
-      assertEquals(7, Day1.part1(testInput))
-    }
+  @Test
+  fun `should test part1`() {
+    val testInput = testInput
+
+    assertEquals(7, Day1.part1(testInput))
+  }
+
+  @Test
+  fun `should test part2`() {
+    val testInput = testInput
+
+    assertEquals(5, Day1.part2(testInput))
+  }
 }
